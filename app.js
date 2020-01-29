f7bc8055fbb8858e19f06f6d55b17fd4cbe68ece
art12354 <35785846+art12354@users.noreply.github.com>
2020-01-29 01:49:06 -0600

    added tabs changed by url

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
-var express = require('express');
-var nunjucks = require('nunjucks');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require("express");
+var nunjucks = require("nunjucks");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
 var app = express();
 
 /* add view engine support */
-app.set('view engine', 'njk')
-nunjucks.configure('views', {
+app.set("view engine", "njk")
+nunjucks.configure("views", {
     autoescape: true,
     express: app
 });
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,24 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.param('tab', function (req, res, next, id) {
+  next();
+});
+router.get('/:tab?', function(req, res, next) {
+  var tab = req.params.tab;
+  if (tab === undefined) {
+    tab = 'basics';
+  }
   res.render('index', {
     title: 'Express',
-    CueData: 'list of cues',
-    InfoData: 'info about current cue'
+    cueData: 'list of cues',
+    infoData: 'info about current cue',
+    infoTab: tab,
+    basicsData: 'Data on Basic options of cue',
+    timeLoopsData: 'Data on Time and Loops options of cue',
+    deviceLevelsData: 'Data on Device and Levels options of cue',
+    trimData: 'Data on Trim options of cue',
+    audioEffectsData: 'Data on Audio Effects options of cue'
   });
 });
 
